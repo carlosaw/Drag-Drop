e62c75b05ff2e49c947101f102d19f9ee154ead9
Carlos Alberto <carlosfreevg@gmail.com>
2021-10-06 14:27:10 -0400

    OK

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let areas = {
+  a: null,
+  b: null,
+  c: null
+};
+
 document.querySelectorAll('.item').forEach(item => {
   item.addEventListener('dragstart', dragStart);// Arrasta
   item.addEventListener('dragend', dragEnd);// Solta
@@ -8,6 +14,10 @@ document.querySelectorAll('.area').forEach(area => {
   area.addEventListener('dragleave', dragLeave);//
   area.addEventListener('drop', drop);// 
 });
+
+document.querySelector('.neutralArea').addEventListener('dragover', dragOverNeutral);
+document.querySelector('.neutralArea').addEventListener('dragleave', dragLeaveNeutral);
+document.querySelector('.neutralArea').addEventListener('drop', dropNeutral);
 
 // Functions Item
 function dragStart(e) {// transparencia qdo arrasta
@@ -20,8 +30,10 @@ function dragEnd(e) {// tira transparencia
 // Functions Area
 function dragOver(e) {
   //console.log("Passou por cima");
-  e.preventDefault();//Libera o drop
-  e.currentTarget.classList.add('hover');//Qdo entra opacity
+  if(e.currentTarget.querySelector('.item') === null) {
+    e.preventDefault();//Libera o drop
+    e.currentTarget.classList.add('hover');//Qdo entra opacity
+  }
 }
 function dragLeave(e) {
   //console.log("SAIU de uma area DROPAVEL");
@@ -29,9 +41,7 @@ function dragLeave(e) {
 }
 function drop(e) {
   //console.log("LIBEROU");
-  e.currentTarget.classList.remove('hover');// Remove transparencia
-
-  
+  e.currentTarget.classList.remove('hover');// Remove transparencia 
 
   // Se não tiver item na area
   if(e.currentTarget.querySelector('.item') === null) {
@@ -40,6 +50,42 @@ function drop(e) {
     //console.log(dragItem);
     //console.log(e.currentTarget);// Área que esta o item
     e.currentTarget.appendChild(dragItem);// Adiciona o item na area
+    updateAreas();
+  }
+}
+
+// Functions NeutralArea
+function dragOverNeutral(e) {
+  e.preventDefault();
+  e.currentTarget.classList.add('hover');
+}
+function dragLeaveNeutral(e) {
+  e.currentTarget.classList.remove('hover');
+}
+function dropNeutral(e) {
+  e.currentTarget.classList.remove('hover');
+  let dragItem = document.querySelector('.item.dragging');  
+  e.currentTarget.appendChild(dragItem);// Adiciona o item na area
+  updateAreas();
+}
+
+
+// Logic functions
+function updateAreas() {
+  document.querySelectorAll('.area').forEach(area=>{
+    let name = area.getAttribute('data-name');
+
+    if(area.querySelector('.item') !== null) {
+      areas[name] = area.querySelector('.item').innerHTML;
+    } else {
+      areas[name] = null;
+    }
+  });
+  //console.log(areas);
+  if(areas.a === '1' && areas.b === '2' && areas.c === '3') {
+    document.querySelector('.areas').classList.add('correct');
+  } else {
+    document.querySelector('.areas').classList.remove('correct');
   }
 }
 
